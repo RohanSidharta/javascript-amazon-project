b03020d809ea5e21d85a7f4232270b0c8e43fafd
RohanSidharta <rohansidharta@gmail.com>
2025-12-24 13:31:02 +0530

    all the product objects are in product.js so we are using 2 scripts

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,34 +1,3 @@
-const products=[{
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    ratings:{
-        stars:4.5,
-        count:87
-    },
-    priceCents:1090
-    
-},
-//this objects represent each product in amazon webpage
-    //it contains all the details like image,name,cost,rating everything in the same structure
-{
-    image: 'images/products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    ratings:{
-        stars:4,
-        count:127
-    },
-    priceCents:2095
-},
-//each details inside {} barkets are product objects which are stored inside and array
-{
-    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: ' Adults Plain Cotton T-Shirt - 2 Pack',
-    ratings:{
-        stars:4.5,
-        count:56
-    },
-    priceCents:799
-}];
 
 let productsHTML = '';
 // now we have to loop through the array using for each loop
