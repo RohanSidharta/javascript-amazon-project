55615bf9afbc76d2b7636a4830b17776896b9c60
RohanSidharta <rohansidharta@gmail.com>
2025-12-25 12:02:30 +0530

    making add to cart button interactive , and checking for matching item in cart to update the quantity of it

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,10 +1,12 @@
 
+
 let productsHTML = '';
 // now we have to loop through the array using for each loop
-//this code below will generate html code for all the three products
-//because we used bactics we can iinsert dynamic values from products array into that html so that each products will get its own html code
+//this code below will generate html code for all the  products
+//because we used bactics we can insert dynamic values from products array into that html so that each products will get its own html code
+//products is an array so we are loopiing it 
 products.forEach((product)=>{  //productsHTML will store the html code in it which we can use it to display in weboage
-     productsHTML = productsHTML + `
+     productsHTML = productsHTML + `  
             <div class="product-container">
             <div class="product-image-container">
                 <img class="product-image"
@@ -17,9 +19,9 @@ products.forEach((product)=>{  //productsHTML will store the html code in it whi
 
             <div class="product-rating-container">
                 <img class="product-rating-stars"
-                src="images/ratings/rating-${product.ratings.stars *10}.png">
+                src="images/ratings/rating-${product.rating.stars *10}.png">
                 <div class="product-rating-count link-primary">
-                ${product.ratings.count}
+                ${product.rating.count}
                 </div>
             </div>
 
@@ -49,15 +51,38 @@ products.forEach((product)=>{  //productsHTML will store the html code in it whi
                 Added
             </div>
 
-            <button class="add-to-cart-button button-primary">
+            <button class="add-to-cart-button button-primary js-add-to-cart"
+            data-product-name="${product.name}">
                 Add to Cart
             </button>
-            </div>`;
+            </div>`
             
-})
+});
 //we have completed 2 steps which are saving data and other is creating html for the data 
 //now coming to the third step which is combining html and put it on the web page
-console.log(productsHTML); // this will combine all the html code of each products together
+ // this will combine all the html code of each products together
 //now last step will be displayig=ng on webpage using DOM
 document.querySelector('.js-products-grid').innerHTML=productsHTML;
 //the above code will insert all the html into the class ja-products-grid so in html we dont need to write that code the javascript will write it for us
+document.querySelectorAll('.js-add-to-cart').forEach((button)=>{ //we are looping add to cart button and when it is clicked the data set in that buttton we will get
+    button.addEventListener('click',()=>{   //this line will add a eventlistener to button whenever it is clicked the dataset of product will be added to the vaiable
+        const productName=button.dataset.productName; //this line will get the product name from button element and store it in variable called productName
+
+        let matchingItem;   //this variable will store the matching items in cart if they matched it will increase the quantity 
+
+        cart.forEach((item)=>{     //it will compare the items in the product.js with the cart array in cart.js for match
+            if(productName===item.productName){  //this line will check whether there are matching items present in cart or not
+                matchingItem=item;      //it will update the matching item in this
+            }
+        });
+        if(matchingItem){         //if any matching item is found in the array of cart then the quantity of that item in incremented
+            matchingItem.quantity +=1;  //quantity in incremented
+        }else{
+            cart.push({            //if the match is not found that means the product is not added to cart from product.js so we will add a new item to the cart array
+                productName: productName,
+                quantity: 1
+            });
+        }
+        console.log(cart);
+    });
+});
