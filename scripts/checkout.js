933a5c701e8361d46bb5da87297f839a49cf9633
RohanSidharta <rohansidharta@gmail.com>
2026-01-14 12:53:50 +0530

    deleted the whole html delement of the product by using dom to retieve it and used .remove to remove it from the page

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -17,7 +17,7 @@ cart.forEach((cartItem)=>{                  //looping through cart arrray to get
     
     
    cartSummaryHTML+= `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
             Delivery date: Tuesday, June 21
             </div>
@@ -104,5 +104,8 @@ document.querySelectorAll('.js-delete-link').forEach((link)=>{
     link.addEventListener('click',()=>{
         const productId=link.dataset.productId;
         removefromCart(productId);
+
+        const container = document.querySelector(`.js-cart-item-container-${productId}`);
+        container.remove();
     });
 });
