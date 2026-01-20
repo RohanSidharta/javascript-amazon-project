ad311ae30a7cfa193b50f9cd6dafda66eaa85ce1
RohanSidharta <rohansidharta@gmail.com>
2026-01-20 13:42:28 +0530

    convert load products from xhr to fetch

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,16 +1,12 @@
 import { renderOrderSummary } from './checkout/orderSummary.js';
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
-import { loadProducts } from '../data/products.js';
+import { loadProducts,loadProductsFetch } from '../data/products.js';
 import { loadCart } from '../data/cart.js';
 //import '../data/cart-class.js';
 //import '../data/backend-practice.js';
 
 Promise.all([
-    new Promise((resolve)=>{             //a promise is same as aa done() method in jasmine it waits until a code is loaded properly then goes to next step using resolve
-        loadProducts(()=>{           //this loads alll the products in the page
-            resolve('value1');              //it is used to wait for all the products to load in the page
-        });
-    }),
+  loadProductsFetch(),
     new Promise((resolve)=>{
         loadCart(()=>{
             resolve('madda gudu');
